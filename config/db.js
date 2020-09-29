c82dc14b6ca839f7cd29b85e9bcb90974790015d
João Victor <joovictordias@hotmail.com>
2020-09-28 22:11:40 -0300

    new commit

---
+++ config/db.js
@@ -0,0 +1,1 @@
+module.exports = {mongoURI:'mongodb+srv://jobWebsite:35327792@cluster0.1gd8d.mongodb.net/JobsWebsite?retryWrites=true&w=majority'}
---
+++ config/session.js
@@ -0,0 +1,1 @@
+module.exports = {secret: '3434557800'}
